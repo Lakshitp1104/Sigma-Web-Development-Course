e98455d63139709088746b699a987febb6777bb0
lakshitp1104 <lakshitpatil13@gmail.com>
2026-07-14 23:21:49 +0530

    js Strings

---
+++ day51/index.js
@@ -0,0 +1,27 @@
+console.log("this is string tutorial")
+let a = "lakshit";
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+
+
+console.log(a.length)
+
+let real_name = "lakshit"
+let friend ="mehul "
+console.log("his name is " + real_name + " and his friends name is " + friend)
+console.log(`his name is ${real_name} and his friends name is ${friend}`)
+
+let b = "lakshit"
+
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,5))
+console.log(b.slice(1))
+console.log(b.replace("la","$$"))
+console.log(b.concat(a , "patil" , "B.tech cse"))
